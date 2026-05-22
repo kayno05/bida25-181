0ec77b2798a109c3fafcf9c7d32b9657289cfb42
kayno05 <Bida25-181@thuto.bac.ac.bw>
2026-05-22 23:07:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("JavaScript is connected!");
+<script>
+const images = document.querySelectorAll('.gallery img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+
+images.forEach(image => {
+  image.addEventListener('click', () => {
+    lightbox.style.display = 'flex';
+    lightboxImg.src = image.src;
+  });
+});
+
+lightbox.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
+
+</script>
